de321bbab6cbb4a4d3282d6bf5c6d9fe41577af1
tomr1233 <165452301+tomr1233@users.noreply.github.com>
2025-05-28 09:20:17 +1200

    Update custom-start.sh

--- scripts/custom-start.sh
+++ scripts/custom-start.sh
@@ -2,11 +2,11 @@
 set -x
 
 # Replace your constants file
-cp /app/constants.ts /app/packages/lib/constants.ts
+cp /constants.ts /app/packages/lib/constants.ts
 
 # Remove the built cache that might contain the old constants
 rm -rf /app/apps/web/.next/cache
 rm -rf /app/apps/web/.next/static
 
 # Run the original startup process
-/app/scripts/start.sh
+/scripts/start.sh
